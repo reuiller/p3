659fcfdca618ae64354bc8c427bdcb6d47cccf48
Maxime <maxime.reuiller@gmail.com>
2017-05-26 19:36:21 +0200

    6

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ function __autoload($class_name){
 	require 'class/'.$class_name.'.php';
 }
 
+// On admet que $db est un objet PDO.
+$test = $bdd->query('SELECT id, title, episode, id_pseudo, content, creation_date FROM episode');
+    
+	return ($test);
+
    $episode = new Episode();
 
     var_dump($episode);
