f13e63e618efc60f2d5875b0f0b85d2f9e5837ed
Maxime <maxime.reuiller@gmail.com>
2017-05-26 19:35:52 +0200

    5

--- class/EpisodeManager.php
+++ class/EpisodeManager.php
@@ -9,5 +9,14 @@ class EpisodeManager{
     $this->setDb($db);
   }
 
-  
+	public function add(Personnage $perso){
+		$q = $this->_db->prepare('INSERT INTO episode(title, episode, id_pseudo, content) VALUES(:title, :episode, :id_pseudo, :content)');
+
+	    $q->bindValue(':title', $episode->title());
+	    $q->bindValue(':episode', $episode->episode();
+	    $q->bindValue(':id_pseudo', $episode->id_pseudo(), PDO::PARAM_INT);
+	    $q->bindValue(':content', $episode->content();
+
+	    $q->execute();
+}
 }
