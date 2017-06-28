68d11d7ff03469780fed1e8a341106cea7caaa63
Maxime <maxime.reuiller@gmail.com>
2017-06-28 11:34:58 +0200

    Initialisation du router

---
+++ router/Router.php
@@ -0,0 +1,15 @@
+<?php
+
+class Router {
+
+	private $url;
+	private $routes = [];
+
+	public function __contruct($url){
+		$this->url = $url;
+	}
+
+	public function get($path, $callable){
+
+	} 
+}
