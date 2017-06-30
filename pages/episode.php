6c37413e5fef071b382dd5c998068b3d207ea5ab
Maxime <maxime.reuiller@gmail.com>
2017-06-30 17:44:47 +0200

    Réorganisation du code

---
+++ pages/episode.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Episode <?= $idEpisode ?></title>
+	</head>
+	<body>
+		Mon episode numéro <?= $idEpisode ?>
+	</body>
+</html>
--- pages/index.php
+++ pages/index.php
@@ -22,6 +22,24 @@ spl_autoload_register('chargerClass');
 	echo $comment->getAuthor();
 
 	var_dump($comment);
+
+
+//Router................
+
+  $url = '';
+if(isset($_GET['url'])) {
+  $url = $_GET['url'];
+}
+
+if($url == ''){
+  require 'home.php';
+} elseif (preg_match('#episode-([0-9]+)#', $url, $params)) {
+  $idEpisode = $params[1];
+  require 'episode.php';
+} else {
+  require '404.php';
+}
+
 ?>
 
 
