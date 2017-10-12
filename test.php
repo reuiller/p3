48e192a91dad380b8afb8b904582f26ec9e3b705
Maxime <maxime.reuiller@gmail.com>
2017-10-12 17:32:31 +0200

    Ajout des test de route dans test.php

--- test.php
+++ test.php
@@ -1,7 +1,14 @@
 <?php
 
-//test
+//test des routes
 
+$router = new Router($_GET['url']); 
+$router->get('/', function($id){ echo "Bienvenue sur ma homepage !</br>"; }); 
+$router->get('/posts/:id', function($id){ echo "Voila l'episode $id"; }); 
+$router->run();
+
+
+//test
 
   $comment = new Comment();    
 	
