dcbe70fd648348eac0bf5bb7cd585afe4a927931
Maxime <maxime.reuiller@gmail.com>
2017-10-12 16:22:33 +0200

    Création de la class Route

---
+++ App/class/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+class Router {
+	
+	private $url; // Contien l'URL sur laquelle on souhaite se rendre
+	private $routes = []; // Contiendra la liste des routes
+
+	public function __construct($url){
+		$this-> = $url;
+	}
+
+}
